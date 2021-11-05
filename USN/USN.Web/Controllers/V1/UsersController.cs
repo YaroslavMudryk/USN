13a56a1dbdd96c93ef3c50758c6269f26284d6a2
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-06 00:09:41 +0200

    Added UsersController

---
+++ USN/USN.Web/Controllers/V1/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace USN.Web.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    public class UsersController : ApiBaseController
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("me")]
+        public IActionResult GetMe()
+        {
+            return Ok();
+        }
+    }
+}
