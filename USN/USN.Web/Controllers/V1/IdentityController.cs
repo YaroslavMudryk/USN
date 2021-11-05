05ab9ddc590a4fdd40ffa260c0e9b8e961184181
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-06 00:05:48 +0200

    Added identity, notification controllers

---
+++ USN/USN.Web/Controllers/V1/IdentityController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+namespace USN.Web.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    public class IdentityController : ApiBaseController
+    {
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+
+        [HttpPost("registration")]
+        public IActionResult Registration()
+        {
+            return Ok();
+        }
+
+        [HttpPost("confirm")]
+        public IActionResult Confirm()
+        {
+            return Ok();
+        }
+
+        [HttpPost("restore")]
+        public IActionResult Restore()
+        {
+            return Ok();
+        }
+
+        [HttpGet("sessions")]
+        public IActionResult GetSessions()
+        {
+            return Ok();
+        }
+
+        [HttpGet("sessions/{id}")]
+        public IActionResult GetSessionById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("sessions/{id}")]
+        public IActionResult CloseSessionById(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ USN/USN.Web/Controllers/V1/NotificationsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace USN.Web.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    public class NotificationsController : ApiBaseController
+    {
+        [HttpGet]
+        public IActionResult GetNotifications(int page = 1)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetNotificationById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpPost("setup")]
+        public IActionResult SetupUserNotification()
+        {
+            return Ok();
+        }
+    }
+}
