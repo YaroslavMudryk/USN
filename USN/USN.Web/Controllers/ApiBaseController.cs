b9bafc33b80c253a2c06bf2961e8f10686ca5fad
Yaroslav Mudryk <yaroslav.mudryk@gmail.com>
2021-11-06 00:04:00 +0200

    Init web project

---
+++ USN/USN.Web/Controllers/ApiBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace USN.Web.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+
+    }
+}
